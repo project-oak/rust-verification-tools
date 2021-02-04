9de2f53d1e9ef6019d5c51dced29e0a187971a74
Alastair Reid <adreid@google.com>
2021-02-04 17:46:27 +0000

    Add copyright header

--- rvt-patch-llvm/src/main.rs
+++ rvt-patch-llvm/src/main.rs
@@ -1,3 +1,11 @@
+// Copyright 2020-2021 The Propverify authors
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use log::info;
 use regex::Regex;
 use std::ffi::OsStr;
