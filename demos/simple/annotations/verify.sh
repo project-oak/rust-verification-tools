0fd0a7ef28a49a36afe2ba06f10d6197732a6a6d
Alastair Reid <adreid@google.com>
2021-02-17 17:21:19 +0000

    Update regression test scripts

--- demos/simple/annotations/verify.sh
+++ demos/simple/annotations/verify.sh
@@ -6,11 +6,11 @@ cargo clean
 
 # verify using KLEE
 # this should detect an error
-( cargo-verify --tests --verbose . > out1 || true )
+( cargo-verify --tests --verbose > out1 || true )
 cat out1
 grep -q "test t1 ... .*ERROR" out1
 
 # replay input values
-( cargo-verify --tests --replay . > out2 || true )
+( cargo-verify --tests --replay > out2 || true )
 cat out2
 grep -q "Test values: a = 1000, b = 1000" out2
