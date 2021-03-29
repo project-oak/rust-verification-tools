8ff591fbdbe968558787de926c9a8cf2d27d45a6
Shaked Flur <sflur@google.com>
2021-03-29 15:00:09 +0100

    Add Using SeaHorn blog post

---
+++ demos/simple/seahorn/verify.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export RUSTFLAGS="-Clto -Cembed-bitcode=yes --emit=llvm-bc $RUSTFLAGS"
+export RUSTFLAGS="--cfg=verify $RUSTFLAGS"
+export RUSTFLAGS="-Warithmetic-overflow -Coverflow-checks=yes $RUSTFLAGS"
+export RUSTFLAGS="-Zpanic_abort_tests -Cpanic=abort $RUSTFLAGS"
+
+# optional for this simple example
+# export RUSTFLAGS="-Copt-level=1 $RUSTFLAGS"
+# export RUSTFLAGS="-Cno-vectorize-loops -Cno-vectorize-slp $RUSTFLAGS"
+# export RUSTFLAGS="-Ctarget-feature=-mmx,-sse,-sse2,-sse3,-ssse3,-sse4.1,-sse4.2,-3dnow,-3dnowa,-avx,-avx2 $RUSTFLAGS"
+
+cargo build --features=verifier-seahorn
+
+rvt-patch-llvm -o try_seahorn.patch.bc --seahorn -vv target/debug/deps/try_seahorn-*.bc
+
+# Find the mangled main function
+MAIN="$(llvm-nm --defined-only try_seahorn.patch.bc | grep main | cut -d ' ' -f3)"
+
+# verify using SeaHorn
+rm -rf seaout
+sea yama -y ${SEAHORN_VERIFY_C_COMMON_DIR}/seahorn/sea_base.yaml bpf --temp-dir=seaout --entry="${MAIN}" try_seahorn.patch.bc
+
+# To get a trace:
+# sea yama -y ${SEAHORN_VERIFY_C_COMMON_DIR}/seahorn/sea_cex_base.yaml bpf --temp-dir=seaout --entry="${MAIN}" try_seahorn.patch.bc
